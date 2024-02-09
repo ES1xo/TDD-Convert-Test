26ab433c204ccb2b29397c2ab026ace6b3d53745
ES1xo <89388456+ES1xo@users.noreply.github.com>
2024-02-09 13:45:07 +0100

    Created Program, Test argument and first test

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static double ConvertInchesToCentimeters(double inches)
+    {
+        return inches * 2.54;
+    }
+
+    public static void Main(string[] args)
+    {
+        TestFunction(() => ConvertInchesToCentimeters(10) == 25.4, "Convert 10 inches to centimeters");
+        TestFunction(() => ConvertInchesToCentimeters(0) == 0, "Convert 0 inches to centimeters");
+        TestFunction(() => ConvertInchesToCentimeters(-1) == -2.54, "Convert -1 inches to centimeters");
+    }
+
+    public static void TestFunction(Func<bool> evaluation, string description)
+    {
+        try
+        {
+            if (evaluation())
+            {
+                // Must be true
+                Console.WriteLine($"🟢 Test:  {description}");
+            }
+            else
+            {
+                Console.WriteLine($"🔴 Test: {description}");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"🔴 Test: {description}");
+        }
+    }
+}
+
