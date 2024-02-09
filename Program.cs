5078f9d06fab7437e5024df5347fba06ac34ccc9
ES1xo <89388456+ES1xo@users.noreply.github.com>
2024-02-09 15:43:50 +0100

    Added argument for running tests or real conversion and tested that it works

--- Program.cs
+++ Program.cs
@@ -9,6 +9,19 @@ public class Program
 
     public static void Main(string[] args)
     {
+        if (args.Length > 0 && args[0] == "-t")
+
+        {
+            runTests(args);
+        }
+        else
+        {
+            Console.WriteLine(ConvertInchesToCentimeters(10));
+        }
+    }
+
+    public static void runTests(string[] args)
+    {
         TestFunction(() => ConvertInchesToCentimeters(10) == 25.4, "Convert 10 inches to centimeters");
         TestFunction(() => ConvertInchesToCentimeters(0) == 0, "Convert 0 inches to centimeters");
         TestFunction(() => ConvertInchesToCentimeters(-1) == -2.54, "Convert -1 inches to centimeters");
